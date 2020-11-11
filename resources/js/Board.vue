7673cb39f43f7db392dd7f531244cf81108ba8b5
mostfaswaisi93 <mostfaswaisi93@gmail.com>
2020-11-11 15:59:20 +0200

    Logging Out

--- resources/js/Board.vue
+++ resources/js/Board.vue
@@ -4,8 +4,9 @@
       <div class="ml-2 w-1/3">x</div>
       <div class="text-lg opacity-50 cursor-pointer hover:opacity-75">Laravello</div>
       <div class="mr-2 w-1/3 flex justify-end">
-        <div v-if="isLoggedIn">
-          <button class="header-btn">Logout</button>
+        <div v-if="isLoggedIn" class="flex items-center">
+          <div class="text-sm mr-2">{{ name }}</div>
+          <button class="header-btn" @click="logout">Logout</button>
         </div>
         <div v-else>
           <button class="header-btn" @click="$router.push({name: 'login'})">Sign-in</button>
@@ -36,18 +37,19 @@
 <script>
 import List from "./components/List";
 import BoardQuery from "./graphql/BoardWithListsAndCards.gql";
+import Logout from "./graphql/Logout.gql";
 import {
   EVENT_CARD_ADDED,
   EVENT_CARD_DELETED,
   EVENT_CARD_UPDATED
 } from "./constants";
+import { mapState } from "vuex";
 export default {
   components: { List },
-  computed: {
-    isLoggedIn() {
-      return this.$store.state.isLoggedIn;
-    }
-  },
+  computed: mapState({
+    isLoggedIn: "isLoggedIn",
+    name: state => state.user.name
+  }),
   apollo: {
     board: {
       query: BoardQuery,
@@ -57,6 +59,14 @@ export default {
     }
   },
   methods: {
+    async logout() {
+      const response = await this.$apollo.mutate({
+        mutation: Logout
+      });
+      if (response.data?.logout?.id) {
+        this.$store.dispatch("logout");
+      }
+    },
     updateQueryCache(event) {
       const data = event.store.readQuery({
         query: BoardQuery,
@@ -83,7 +93,6 @@ export default {
   }
 };
 </script>
-
 <style scoped>
 .header {
   height: 40px;
